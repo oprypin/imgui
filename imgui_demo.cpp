9d6a93fe2b50e79374b9b6406841c98a508550cf
Oleh Prypin <oleh@pryp.in>
2020-09-10 00:20:39 +0200

    Report the actual size of the text

--- imgui_demo.cpp
+++ imgui_demo.cpp
@@ -1269,7 +1269,9 @@ static void ShowDemoWindowWidgets()
                     {
                         ImVector<char>* my_str = (ImVector<char>*)data->UserData;
                         IM_ASSERT(my_str->begin() == data->Buf);
-                        my_str->resize(data->BufSize); // NB: On resizing calls, generally data->BufSize == data->BufTextLen + 1
+                        // NB: On resizing calls, generally data->BufSize == data->BufTextLen + 1
+                        my_str->resize(data->BufSize); // Use resize to take advantage of capacity prediction.
+                        my_str->shrink(data->BufTextLen); // Get the reported size back to the actual one.
                         data->Buf = my_str->begin();
                     }
                     return 0;
@@ -1280,7 +1282,11 @@ static void ShowDemoWindowWidgets()
                 static bool MyInputTextMultiline(const char* label, ImVector<char>* my_str, const ImVec2& size = ImVec2(0, 0), ImGuiInputTextFlags flags = 0)
                 {
                     IM_ASSERT((flags & ImGuiInputTextFlags_CallbackResize) == 0);
-                    return ImGui::InputTextMultiline(label, my_str->begin(), (size_t)my_str->size(), size, flags | ImGuiInputTextFlags_CallbackResize, Funcs::MyResizeCallback, (void*)my_str);
+                    if (my_str->empty()) {
+                        my_str->push_back(0);
+                        my_str->pop_back();
+                    }
+                    return ImGui::InputTextMultiline(label, my_str->begin(), (size_t)my_str->capacity(), size, flags | ImGuiInputTextFlags_CallbackResize, Funcs::MyResizeCallback, (void*)my_str);
                 }
             };
 
@@ -1288,8 +1294,6 @@ static void ShowDemoWindowWidgets()
             // Note that because we need to store a terminating zero character, our size/capacity are 1 more
             // than usually reported by a typical string class.
             static ImVector<char> my_str;
-            if (my_str.empty())
-                my_str.push_back(0);
             Funcs::MyInputTextMultiline("##MyStr", &my_str, ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 16));
             ImGui::Text("Data: %p\nSize: %d\nCapacity: %d", (void*)my_str.begin(), my_str.size(), my_str.capacity());
             ImGui::TreePop();
